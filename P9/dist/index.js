8c5d335d2ca653bdaff45fb33bea8d921feb1670
plnkhanh1609 <plnkhanh1609@gmail.com>
2024-03-26 20:19:29 +0700

    #9

---
+++ P9/dist/index.js
@@ -0,0 +1,14 @@
+const modalHandlerLogin = (modal) => {
+    document.querySelector(modal).classList.remove("hidden");
+};
+const removeModalLogin = (modal) => {
+    console.log(modal);
+    document.querySelector(modal).classList.add("hidden");
+};
+const modalHandlerCart = (modal) => {
+    document.querySelector(modal).classList.remove("hidden");
+};
+const checkoutback = (modal) => {
+    console.log(modal);
+    document.querySelector(modal).classList.add("hidden");
+};
